c769438e743dd1ed553b22dbe50391820bf9708f
WebFreak001 <gh@webfreak.org>
2022-09-12 21:36:44 +0200

    use proper lib paths on linux

--- src/compiler.ts
+++ src/compiler.ts
@@ -1,4 +1,5 @@
 import * as tc from '@actions/tool-cache';
+import { cmpSemver, parseSimpleSemver } from './semver';
 import { body_as_text } from './utils';
 
 export interface CompilerDescription {
@@ -6,7 +7,7 @@ export interface CompilerDescription {
     version: string;
     url: string;
     binpath: string;
-    libpath : string[];
+    libpath: string[];
     sig?: string;
     dub?: DubDescription;
 }
@@ -221,15 +222,25 @@ async function ldc_resolve_master(gh_token: string): Promise<CompilerDescription
         });
 
     const latest = assets[0];
+
     const base_path = (process.platform == "win32") ?
         `\\ldc2-${latest.version}-${suffix}\\` :
         `/ldc2-${latest.version}-${suffix}/`;
+    let libpath: string[] = [];
+    switch (process.platform) {
+        case "win32": libpath = [`${base_path}lib64`]; break;
+        case "linux": libpath = [`${base_path}lib`, `${base_path}lib64`]; break;
+        case "darwin": libpath = [`${base_path}lib-arm64`, `${base_path}lib-x86_64`]; break;
+        default:
+            throw new Error("unsupported platform: " + process.platform);
+    }
+
     return {
         name: "ldc2",
         version: latest.version,
         url: "https://github.com/ldc-developers/ldc/releases/download/CI/" + latest.name,
         binpath: `${base_path}bin`,
-        libpath: [ `${base_path}lib64` ]
+        libpath: libpath
     };
 }
 
@@ -256,17 +267,25 @@ async function ldc(version: string, dub_vers: string, gh_token: string): Promise
             version: version,
             url: `${base_url}-windows-multilib.7z`,
             binpath: `\\ldc2-${version}-windows-multilib\\bin`,
-            libpath: [ `\\ldc2-${version}-windows-multilib\\lib64` ],
+            libpath: [`\\ldc2-${version}-windows-multilib\\lib64`],
             dub: await dub(dub_vers, gh_token, false)
         };
-        case "linux": return {
-            name: "ldc2",
-            version: version,
-            url: `${base_url}-linux-x86_64.tar.xz`,
-            binpath: `/ldc2-${version}-linux-x86_64/bin`,
-            libpath: [ `/ldc2-${version}-linux-x86_64/lib64` ],
-            dub: await dub(dub_vers, gh_token, false)
-        };
+        case "linux":
+            var arch: string = "";
+            switch (process.arch) {
+                case "ia32": arch = "x86"; break; // supported on very old LDC releases
+                case "x64": arch = "x86_64"; break;
+                case "arm": arch = "armhf"; break; // supported on old LDC releases
+                case "arm64": arch = "aarch64"; break;
+            }
+            return {
+                name: "ldc2",
+                version: version,
+                url: `${base_url}-linux-${arch}.tar.xz`,
+                binpath: `/ldc2-${version}-linux-${arch}/bin`,
+                libpath: [`/ldc2-${version}-linux-${arch}/lib`, `/ldc2-${version}-linux-${arch}/lib64`],
+                dub: await dub(dub_vers, gh_token, false)
+            };
         case "darwin":
             var arch: string = "";
             switch (process.arch) {
